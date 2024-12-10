27a7745a5686bed8c9f0072932ad92939643930a
Dimuthu Rathnayaka <info.dlrathnayaka@gmail.com>
2024-12-11 01:46:21 +0530

    add movo_text.svg file

---
+++ frontend/src/components/Dimuthu/Dimuthu.jsx
@@ -0,0 +1,15 @@
+import "../Dimuthu/Dimuthu.css"
+import logo from "../../../src/assets/images/MOVO.png"
+
+const Dimuthu = () => {
+    return (
+        <div className="container">
+           <img className="logo"
+           src={logo} alt="move"/>
+
+           
+        </div>
+    );
+};
+
+export default Dimuthu;

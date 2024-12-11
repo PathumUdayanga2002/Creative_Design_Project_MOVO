2aae8178a5dd4dc841f9a5c782f237d19152eb22
Dimuthu Rathnayaka <info.dlrathnayaka@gmail.com>
2024-12-12 00:06:02 +0530

    call header admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PresenterDashBoard from "./components/PresenterDashboard/PresenterDashBoard";
 import Dimuthu from "./components/Dimuthu/dimuthu";
+import Header from "./components/Header/Header";
+import HeaderAdmin from "./components/HeaderAdmin/HeaderAdmin";
 
 const App = () => {
   return <div>
-    <Dimuthu/>
+    <HeaderAdmin/>
   </div>;
 };
 
